acef4022c1bc85d34f719a4cdab66e9288428ca2
Gelu Bolba <gelubolba0@gmail.com>
2024-08-28 15:12:53 +0300

    Remove node_modules from the repository

--- cypress/e2e/Orders/completeOrder.cy.js
+++ cypress/e2e/Orders/completeOrder.cy.js
@@ -15,30 +15,25 @@ describe('Complete Order Flow with Multiple Products', () => {
     homePage.visit();
     homePage.searchProduct(testData.products[0].name);
     homePage.selectProductFromResults(testData.products[0].name);
-
     productPage.selectSize(testData.products[0].size);
     productPage.selectColor(testData.products[0].color);
     productPage.adjustQuantity(2);
     productPage.addToCart();
-
     homePage.searchProduct(testData.products[1].name);
     homePage.selectProductFromResults(testData.products[1].name);
     productPage.selectSize(testData.products[1].size);
     productPage.selectColor(testData.products[1].color);
     productPage.addToCart();
     productPage.goToCart();
-
     cartPage.checkCartTotal('$163.00');
     cartPage.proceedToCheckout();
-
     checkoutPage.fillShippingDetails(testData.shippingDetails);
     checkoutPage.selectShipping(0);
     checkoutPage.clickOnNextButton();
     checkoutPage.placeOrder();
-
     orderConfirmationPage.verifyOrderSuccess();
     orderConfirmationPage.getOrderNumber().then((orderNumber) => {
-      cy.log(`Order Number: ${orderNumber}`);
+    cy.log(`Order Number: ${orderNumber}`);
     });
   });
 });
